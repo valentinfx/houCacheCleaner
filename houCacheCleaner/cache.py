26bc591be51a52477e8bb29b17148e9baf448d6b
vdavidcg <vdavid.pro@gmail.com>
2025-03-15 12:05:31 +0100

    Added error handling

--- houCacheCleaner/cache.py
+++ houCacheCleaner/cache.py
@@ -1,23 +1,24 @@
 
 import os
 import re
+from typing import List
 
 import hou
 
 from houCacheCleaner.cache_version import CacheVersion
-from houCacheCleaner.common import FILE_CACHE_NODE_TYPE_NAME, truncate
+from houCacheCleaner.common import FILE_CACHE_NODE_TYPE_NAME
 
 
 class Cache:
-    """Object defining a cache and all its curent versions"""
+    """Object defining a cache and all its current versions"""
 
-    def __init__(self, name: str = None, version_paths: list[str] = None):
+    def __init__(self, name: str = None, version_paths: List[str] = []):
         """
-        :param name: Name of the cache
+        :param version_paths: A list of all the versions of this cache
         :param versions: A list of all the versions of this cache
         """
         self.name = name
-        self.versions = []
+        self.versions = list()
         self.file_cache_node = None
         self.latest = None
         self.last_three = None
@@ -42,13 +43,14 @@ class Cache:
         """Update version list for this cache"""
         self.versions.clear()
 
-        for versions_path in version_paths:
-            # We'll create a CacheVersion instance for each version
-            self.versions.append(CacheVersion(versions_path))
+        if version_paths is not None:
+            for versions_path in version_paths:
+                # We'll create a CacheVersion instance for each version
+                self.versions.append(CacheVersion(versions_path))
 
         # Let's sort the versions by version number
         self.versions = sorted(
-            self.versions, key=lambda CacheVersion:CacheVersion.version_nbr, reverse=True)
+            self.versions, key=lambda cache_version: cache_version.version_nbr, reverse=True)
 
     def get_versions(self) -> list[CacheVersion]:
         return self.versions
@@ -72,11 +74,11 @@ class Cache:
         # and set the attribute to None
         else:
             self.file_cache_node = None
-            text = f'Found {len(candidates)} nodes with name {self.name} at \n'
+            text = f"Found {len(candidates)} nodes with name {self.name} at \n"
             for i in candidates:
-                text += f'{i.path()}\n'
+                text += f"{i.path()}\n"
 
-            title = 'Found duplicate filecache nodes'
+            title = "Found duplicate filecache nodes"
 
             hou.ui.displayConfirmation(
                 text=text,
@@ -98,13 +100,16 @@ class Cache:
 
     def get_disk_size(self):
         return self.size_on_disk
-
+    
     def set_latest(self):
         """Find the latest version for this cache"""
-        latest = self.get_versions()[0]
-        latest.is_latest = True
-        self.latest = latest
-
+        versions = self.get_versions()
+        if versions:
+            latest = versions[0]
+            latest.is_latest = True
+            self.latest = latest
+        else:
+            self.latest = None
 
     def get_latest(self) -> CacheVersion:
         """Get the latest version for this cache"""
@@ -112,12 +117,12 @@ class Cache:
 
     def set_last_three(self):
         """Find the last three versions for this cache"""
-        last_three = (v for v in self.get_versions()[:3])
+        last_three = [v for v in self.get_versions()[:3]]
         for v in last_three:
             v.isLastThree = True
         self.last_three = last_three
 
-    def get_last_three(self) -> tuple[CacheVersion]:
+    def get_last_three(self) -> list[CacheVersion]:
         """Get the last three versions for this cache"""
         return self.last_three
 
